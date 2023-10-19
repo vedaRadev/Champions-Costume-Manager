b109aea54277f3783e4e08399adcbe593ee86c5f
vedaRadev <ryancarmstrong97@gmail.com>
2023-10-19 17:00:32 -0400

    basic ui structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{ fs, path::{ Path, PathBuf } };
+use std::{ fs, path::Path };
 use eframe::egui;
+// use rfd::FileDialog;
 
 const COSTUME_SAVES_PATH: &str = "/mnt/c/Program Files (x86)/Steam/steamapps/common/Champions Online/Champions Online/Live/screenshots";
 
@@ -7,50 +8,79 @@ const COSTUME_SAVES_PATH: &str = "/mnt/c/Program Files (x86)/Steam/steamapps/com
 // * Only return files that are valid costume saves.
 // * Ensure this is performant when the dir contains many files.
 // * Actual error handling, maybe return a result containing the vec.
-fn get_saved_costumes(saves_dir: &Path) -> Vec<PathBuf> {
-    fs::read_dir(saves_dir).unwrap().map(|p| p.unwrap().path()).collect()
+fn get_saved_costumes(saves_dir: &Path) -> Vec<String> {
+    fs::read_dir(saves_dir).unwrap()
+        .map(|p| String::from(p.unwrap().path().to_str().unwrap()))
+        .collect()
 }
 
 fn main() -> Result<(), eframe::Error> {
-    let costumes = get_saved_costumes(Path::new(COSTUME_SAVES_PATH));
-    println!("{:?}", costumes);
-
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(800.0, 600.0)),
         ..Default::default()
     };
 
     eframe::run_native(
-        "Test App",
+        "Champions Costume Manager",
         options,
         Box::new(|cc| {
             egui_extras::install_image_loaders(&cc.egui_ctx);
 
-            Box::new(ChampionsCostumeManager::new(costumes))
+            Box::new(ChampionsCostumeManager::new(cc))
         })
     )
 }
 
 struct ChampionsCostumeManager {
-    costume_uris: Vec<String>,
+    costume_saves: Vec<String>
 }
 
 impl ChampionsCostumeManager {
-    fn new(costume_paths: Vec<PathBuf>) -> Self {
-        ChampionsCostumeManager {
-            costume_uris: costume_paths.into_iter()
-                .map(|p| format!("file://{}", p.to_str().unwrap()))
-                .collect()
+    fn new(_cc: &eframe::CreationContext) -> Self {
+        Self {
+            costume_saves: get_saved_costumes(Path::new(COSTUME_SAVES_PATH))
         }
     }
 }
 
 impl eframe::App for ChampionsCostumeManager {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // IMAGE DETAILS PANEL
+        egui::SidePanel::right("costume_preview_panel")
+            .resizable(false)
+            .min_width(250.0)
+            .show(ctx, |ui| {
+                ui.add(
+                    egui::Image::new(format!("file://{}", &self.costume_saves[0]))
+                        .rounding(10.0)
+                        .maintain_aspect_ratio(true)
+                        .shrink_to_fit()
+                );
+
+                egui::TopBottomPanel::top("costume_details_panel").show_inside(ui, |ui| {
+                    ui.label("Placeholder Name");
+                    ui.label("Placeholder Character");
+                    ui.label("Placeholder Owner");
+                });
+            });
+
+        // COSTUME SELECTION
         egui::CentralPanel::default().show(ctx, |ui| {
-            for uri in &self.costume_uris {
-                ui.add(egui::Image::new(uri));
-            }
+            let panel_width = ui.available_width();
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                ui.set_min_width(panel_width);
+
+                ui.horizontal_wrapped(|ui| {
+                    for costume in &self.costume_saves {
+                        ui.add(
+                            egui::Image::new(format!("file://{}", costume))
+                                .rounding(10.0)
+                                .fit_to_original_size(0.5)
+                                .maintain_aspect_ratio(true)
+                        );
+                    }
+                });
+            });
         });
     }
 }
